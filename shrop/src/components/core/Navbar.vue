b50fb85875ab2fc3b1852a5e7cb3fa2cf0af3b96
Donovan <donovan.hoang@viacesi.fr>
2022-06-14 15:44:28 +0200

    fix link in navbar

--- shrop/src/components/core/Navbar.vue
+++ shrop/src/components/core/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar has-background-success-dark" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
+        <a class="navbar-item" href="http://localhost:8080/#/inventory">
           <img src="../../assets/shroplogo.jpg" width="112" height="112">
         </a>
         <a class="navbar-item has-text-light">Get out of my swamp Grrrrrrreeeeeeee !!!!</a>
